a25da758d7fc398fcee5c8893476d4483c010a9a
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-06-16 13:55:50 -0300

    configure db context

--- dev-habit/DevHabit/Database/ApplicationDbContext.cs
+++ dev-habit/DevHabit/Database/ApplicationDbContext.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DevHabit.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevHabit.Database;
 
 public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
+
+    public DbSet<Habit> Habits { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema(Schemas.Application);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
     }
 
 }
