647caef67c37647853c08cac831f9dd6e41730ab
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-06-17 11:53:13 -0300

    add post request

---
+++ dev-habit/DevHabit/DTOs/Habits/CreateHabitDto.cs
@@ -0,0 +1,15 @@
+﻿using DevHabit.Entities;
+
+namespace DevHabit.DTOs.Habits;
+
+public sealed record CreateHabitDto
+{   
+    public required string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public required HabitType Type { get; set; }
+    public required FrequencyDto Frequency { get; set; }
+    public required TargetDto Target { get; set; }  
+    public DateOnly? EndDate { get; set; }
+    public MilestoneDto? Milestone { get; set; }
+   
+}
