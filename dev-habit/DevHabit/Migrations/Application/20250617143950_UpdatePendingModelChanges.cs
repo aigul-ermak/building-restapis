a23f694c491a5a1888840503f6f0d740a59b0790
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-06-17 11:51:47 -0300

    add migration: changed schema for habit

---
+++ dev-habit/DevHabit/Migrations/Application/20250617143950_UpdatePendingModelChanges.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevHabit.Migrations.Application;
+
+/// <inheritdoc />
+public partial class UpdatePendingModelChanges : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<int>(
+            name: "frequency_times_per_period",
+            schema: "dev_habit",
+            table: "habits",
+            type: "integer",
+            nullable: false,
+            defaultValue: 0);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "frequency_times_per_period",
+            schema: "dev_habit",
+            table: "habits");
+    }
+}
